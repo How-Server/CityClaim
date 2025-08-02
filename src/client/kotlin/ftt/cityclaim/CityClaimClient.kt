422ef00be39ee5ba7f0a99bd1a5fe1689165c14b
Howie <howie0715@gmail.com>
2025-08-02 19:40:30 +0800

    clear: necessary package

--- src/client/kotlin/ftt/cityclaim/CityClaimClient.kt
+++
@@ -1,9 +0,0 @@
-package ftt.cityclaim
-
-import net.fabricmc.api.ClientModInitializer
-
-object CityClaimClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
